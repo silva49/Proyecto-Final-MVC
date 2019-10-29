20076fc8b7bd57e0efbbd173b66734a7eee4deb1
cristian david tilano silva <cristiantilanosilva@gmail.com>
2019-10-28 20:12:24 -0500

    se inicia el taller final con la conexión a la base de datos que se va a utilizar

---
+++ FinalMVC/Controlador/ClsContactoMgr.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using Modelo;
+
+
+namespace Controlador
+{
+    public class ClsContactoMgr
+    {
+        ClsDatos cnGeneral = null;
+        ClsContacto objContacto = null;
+        DataTable tblDatos = null;
+
+        public ClsContactoMgr(ClsContacto parObjContacto)
+        {
+            objContacto = parObjContacto;
+        }
+
+        public DataTable Ingresar()
+        {
+            tblDatos = new DataTable();
+
+            try
+            {
+                cnGeneral = new ClsDatos();
+                SqlParameter[] parParameter = new SqlParameter[1];
+                parParameter[0] = new SqlParameter();
+                parParameter[0].ParameterName = "@opc";
+                parParameter[0].SqlDbType = SqlDbType.Int;
+                parParameter[0].SqlValue = objContacto.opc;
+
+                tblDatos = cnGeneral.RetornaTabla(parParameter, "SPContacto");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return tblDatos;
+        }
+
+
+        public void GuardarDatos()
+        {
+            try
+            {
+                cnGeneral = new ClsDatos();
+                SqlParameter[] parParameter = new SqlParameter[4];
+
+                parParameter[0] = new SqlParameter();
+                parParameter[0].ParameterName = "@opc";
+                parParameter[0].SqlDbType = SqlDbType.Int;
+                parParameter[0].SqlValue = objContacto.opc;
+
+                parParameter[1] = new SqlParameter();
+                parParameter[1].ParameterName = "@Id";
+                parParameter[1].SqlDbType = SqlDbType.Int;
+                parParameter[1].SqlValue = objContacto.Id;
+
+
+                parParameter[2] = new SqlParameter();
+                parParameter[2].ParameterName = "@Nombre";
+                parParameter[2].SqlDbType = SqlDbType.VarChar;
+                parParameter[2].Size = 50;
+                parParameter[2].SqlValue = objContacto.Nombre;
+
+                
+
+                cnGeneral.EjecutarSP(parParameter, "SPContacto");
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        public void EliminarDatos()
+        {
+            try
+            {
+                cnGeneral = new ClsDatos();
+                SqlParameter[] parParameter = new SqlParameter[2];
+
+                parParameter[0] = new SqlParameter();
+                parParameter[0].ParameterName = "@opc";
+                parParameter[0].SqlDbType = SqlDbType.Int;
+                parParameter[0].SqlValue = objContacto.opc;
+
+                parParameter[1] = new SqlParameter();
+                parParameter[1].ParameterName = "@Id";
+                parParameter[1].SqlDbType = SqlDbType.Int;
+                parParameter[1].SqlValue = objContacto.Id;
+
+                cnGeneral.EjecutarSP(parParameter, "SPContacto");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+    }
+
+}
+
+
---
+++ FinalMVC/Modelo/ClsDatos.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Modelo
+{
+   public  class ClsDatos
+    {
+
+        #region declaración de variables
+
+        SqlConnection cnnConexion = null;
+        SqlCommand cmdComando = null;
+        SqlDataAdapter daAdaptador = null;
+        DataTable Dtt = null;
+        string strCadenaConexion = string.Empty;
+        #endregion
+
+        #region Constructor
+        public ClsDatos()
+        {
+            strCadenaConexion = @"Data Source=SALA402-3\SQLEXPRESS;Initial Catalog=Colegio;Integrated Security=True";
+        }
+        #endregion
+
+        #region Métodos a ejecutar
+
+        public void EjecutarSP(SqlParameter[] parParametros, string pasSPName)
+        {
+            try
+            {
+                cnnConexion = new SqlConnection(strCadenaConexion);
+                cmdComando = new SqlCommand();
+                cmdComando.Connection = cnnConexion;
+                cnnConexion.Open();
+                cmdComando.CommandType = CommandType.StoredProcedure;
+                cmdComando.CommandText = pasSPName;
+                cmdComando.Parameters.AddRange(parParametros);
+                cmdComando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                cnnConexion.Dispose();
+                cmdComando.Dispose();
+            }
+        }
+
+
+        public DataTable RetornaTabla(SqlParameter[] parParametros, string parTSQL)
+        {
+            Dtt = null;
+            try
+            {
+                Dtt = new DataTable();
+                cnnConexion = new SqlConnection(strCadenaConexion);
+                cmdComando = new SqlCommand();
+                cmdComando.Connection = cnnConexion;
+                cmdComando.CommandType = CommandType.StoredProcedure;
+                cmdComando.CommandText = parTSQL;
+                cmdComando.Parameters.AddRange(parParametros);
+
+                daAdaptador = new SqlDataAdapter(cmdComando);
+                daAdaptador.Fill(Dtt);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+
+
+            }
+            finally
+            {
+                cnnConexion.Dispose();
+                cmdComando.Dispose();
+                daAdaptador.Dispose();
+            }
+
+            return Dtt;
+        }
+
+
+        #endregion
+
+
+    }
+}
